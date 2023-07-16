20bf597e671de6132a69f94c6208fb521e1fdab0
Nathan Fabio <nathan_3_m@hotmail.com>
2023-07-15 22:20:40 -0300

    feat: creating user objects

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/nathanfabio/crudGo/src/controller/routes"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
 	}
 }
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,21 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nathanfabio/crudGo/src/configuration/rest_err"
+	"github.com/nathanfabio/crudGo/src/controller/model/request"
+)
 
 func CreateUser(c *gin.Context) {
-	
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("Incorrect fields, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+	}
+
+	fmt.Println(userRequest)
 }
